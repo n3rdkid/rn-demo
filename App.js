52900ec69848e9e4b0a57140bdc07a1efd76e617
Saurav Adhikari <sauravads123@gmail.com>
2021-02-11 12:44:36 +0545

    feat : load data from firebase

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
-
+import {db} from "./config";
 export default function App() {
-  const [description,setDescription]=useState("");
+  const [shopItems,setShopItems]=useState([]);
+  const [isLoading,setIsLoading]=useState(true);
+  useEffect(()=>{
+    fetchData()
+
+  },[])
+
+  const fetchData =async ()=>{
+    const response= db.collection('collections')
+    const data = await response.get();
+    return convertCollectionsSnapshotToMap(data)
+  }
+
+  const convertCollectionsSnapshotToMap = collections => {
+    const transformedCollections = collections.docs.map(doc => {
+      const { title, items } = doc.data();
+      return { id: doc.id, title, route: encodeURI(title.toLowerCase()), items };
+    });
+    return transformedCollections.reduce((accmulator, collection) => {
+      accmulator[collection.title.toLowerCase()] = collection;
+      return accmulator;
+    }, {});
+  };
+  
+
+
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={{height: 40}}
-        placeholder="Type here to do something"
-        onChangeText={text => setDescription(text)}
-        defaultValue={description}
-      />
-      <Text>{description}</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={{flex: 1, flexDirection: 'column'}}>
+    <View style={{backgroundColor: 'powderblue'}} />
+    <View style={{backgroundColor: 'skyblue'}} />
+    <View style={{backgroundColor: 'steelblue'}} />
+  </View>
   );
 }
 
